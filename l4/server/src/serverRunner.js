1ad080753a40c104eae4bf08c1f10b5c17a736a1
Les Holikov <sanyagolikov97@gmaail.com>
2024-05-04 15:48:11 +0300

    Set logger and write comments

--- l4/server/src/serverRunner.js
+++ l4/server/src/serverRunner.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import userRouter from './user/UserRouter.js';
+import morgan from 'morgan';
+import UserRouter from './user/UserRouter.js';
 import { UserController } from './user/UserController.js';
 
+/**
+ * Function to run the server.
+ * @param {number} port - The port number on which the server should listen.
+ */
 const serverRunner = async (port) => {
+  // Create an Express server instance
   const server = express();
+
+  // Middleware to parse incoming request bodies as JSON
   server.use(express.json());
 
-  const dbPath = './src/user/data/users_db.json';
-  const userController = new UserController(dbPath);
-  server.use('/users', new userRouter(userController).routes);
+  // Middleware for logging HTTP requests
+  server.use(morgan('dev'));
 
+  // Initialize user-related routes
+  const dbPath = './user/data/users_db.json';
+  const userController = new UserController(dbPath);
+  const userRouter = new UserRouter(userController);
+  server.use('/users', userRouter.routes);
+
+  // Start the server and listen on the specified port
   server.listen(port, () => {
     console.log(`🦾 Server was runned on http://127.0.0.1:${port}`);
   });
--- l4/server/src/user/UserRouter.js
+++ l4/server/src/user/UserRouter.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { UserController } from './UserController.js';
 
+/**
+ * Router class for handling user-related routes.
+ */
 class UserRouter {
+  /**
+   * Constructs a new UserRouter instance.
+   * @param {UserController} controller - The user controller instance.
+   * @throws {Error} Throws an error if the provided controller is not an instance of UserController.
+   */
   constructor(controller) {
-    if (!(controller instanceof UserController)) {
+    if (!controller instanceof UserController) {
       throw new Error('⛔ UserRouter expects UserController');
     }
 
@@ -12,28 +20,39 @@ class UserRouter {
     this.initializeRoutes();
   }
 
+  /**
+   * Initializes user-related routes.
+   */
   initializeRoutes() {
-    this.routes.get('/', (req, res) => this.controller.getAllUsers(req, res));
+    // Route for getting all users
+    this.routes.get('/', (req, res) => {
+      return this.controller.getAllUsers(req, res);
+    });
 
-    this.routes.get('/:id', (req, res) =>
-      this.controller.getUserById(req, res)
-    );
+    // Route for getting a user by ID
+    this.routes.get('/:id', (req, res) => {
+      return this.controller.getUserById(req, res);
+    });
 
-    this.routes.post('/', (req, res) =>
-      this.controller.createNewUser(req, res)
-    );
+    // Route for creating a new user
+    this.routes.post('/', (req, res) => {
+      return this.controller.createNewUser(req, res);
+    });
 
-    this.routes.patch('/:id', (req, res) =>
-      this.controller.updateUser(req, res)
-    );
+    // Route for updating a user by ID
+    this.routes.patch('/:id', (req, res) => {
+      return this.controller.updateUser(req, res);
+    });
 
-    this.routes.delete('/:id', (req, res) =>
-      this.controller.deleteUserById(req, res)
-    );
+    // Route for deleting a user by ID
+    this.routes.delete('/:id', (req, res) => {
+      return this.controller.deleteUserById(req, res);
+    });
 
-    this.routes.delete('/', (req, res) =>
-      this.controller.deleteAllUsers(req, res)
-    );
+    // Route for deleting all users
+    this.routes.delete('/', (req, res) => {
+      return this.controller.deleteAllUsers(req, res);
+    });
   }
 }
 
